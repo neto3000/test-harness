0be58931b137e0456bf648699383d183af7d7d18
Hugo Lopez <hugo.lopez@bigcommerce.com>
2019-06-19 12:39:58 -0500

    Initial migration

---
+++ db/migrate/20190619171003_create_initial_tables.rb
@@ -0,0 +1,72 @@
+class CreateInitialTables < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.index :email, unique: true
+      t.timestamps
+    end
+
+    create_table :projects do |t|
+      t.string :name
+
+      t.index :name, unique: true
+      t.timestamps
+    end
+
+    create_table :tests do |t|
+      t.string :name
+      t.text :description
+      t.text :spec
+
+      t.index :name, unique: true
+      t.timestamps
+    end
+
+    create_table :suite_tests do |t|
+      t.string :name
+      t.text :description
+
+      t.index :name, unique: true
+      t.timestamps
+    end
+
+    create_table :test_runs do |t|
+      t.string :execution_time
+      t.string :status
+      t.text :warning
+      t.text :success
+      t.text :failures
+
+      t.timestamps
+    end
+
+    create_table :suite_test_runs do |t|
+      t.string :execution_time
+      t.string :status
+      t.text :warning
+      t.text :success
+      t.text :failures
+
+      t.timestamps
+    end
+
+    def change
+      add_reference :projects, :user, foreign_key: true
+      add_reference :tests, :user, foreign_key: true
+      add_reference :suite_tests, :user, foreign_key: true
+      add_reference :test_runs, :user, foreign_key: true
+      add_reference :suite_test_runs, :user, foreign_key: true
+
+      add_reference :tests, :project, foreign_key: true
+      add_reference :suite_tests, :project, foreign_key: true
+      add_reference :test_runs, :project, foreign_key: true
+      add_reference :suite_test_runs, :project, foreign_key: true
+
+      add_reference :tests, :suite_test, foreign_key: true
+      add_reference :test_runs, :suite_test, foreign_key: true
+
+    end
+  end
+end
